76bda5a03074caf71f39390801eef6f3182724c4
Dragos Nichifor <dragosnichifor14@gmail.com>
2026-02-03 19:01:22 +0200

    ✨ Move professor activity feedback feature in separate folder

--- frontend/src/app/features/professor/professor.module.ts
+++ frontend/src/app/features/professor/professor.module.ts
@@ -13,7 +13,7 @@ import { ActivityService, CourseService } from '$backend/services';
 import { DialogPageComponent } from '$shared/dialog-page';
 import { ProfessorAddActivityComponent } from './courses/professor-add-activity.component';
 import { ProfessorUpdateActivityComponent } from './courses/professor-update-activity.component';
-import { ProfessorActivityFeedbackComponent } from './professor-activity-feedback/professor-activity-feedback.component';
+import { ProfessorActivityFeedbackComponent } from './feedback/professor-activity-feedback/professor-activity-feedback.component';
 
 const PROFESSOR_ROUTES: Routes = [
   {
